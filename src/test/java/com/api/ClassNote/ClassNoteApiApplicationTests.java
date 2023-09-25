1d00600778b3c1a947f7ba845e4e469747229f80
Leopoldo Jacob Matsinhe <121976617+l0m2@users.noreply.github.com>
2023-09-25 12:46:02 +0200

    first commit

---
+++ src/test/java/com/api/ClassNote/ClassNoteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ClassNote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassNoteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
