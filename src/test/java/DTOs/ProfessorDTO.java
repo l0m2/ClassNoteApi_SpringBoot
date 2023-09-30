41d7ca17c1da7964fe1e4a3983fef6e1f7530bb5
Leopoldo Jacob Matsinhe <121976617+l0m2@users.noreply.github.com>
2023-09-30 14:18:14 +0200

    ProfessorDTO

---
+++ src/test/java/DTOs/ProfessorDTO.java
@@ -0,0 +1,38 @@
+package DTOs;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class ProfessorDTO {
+
+	@NotBlank
+	@Size(min=2)
+	private String nome;
+	@Email
+	private String email;
+	@NotNull
+	private String telefone;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+}
