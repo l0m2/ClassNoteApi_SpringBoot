7f6cc2391de7cd6eb51c355feadb61c01bf5bd5a
Leopoldo Jacob Matsinhe <121976617+l0m2@users.noreply.github.com>
2023-10-02 21:04:19 +0200

    get professor

--- src/main/java/com/api/ClassNote/Controller/ProfessorController.java
+++ src/main/java/com/api/ClassNote/Controller/ProfessorController.java
@@ -1,13 +1,17 @@
 package com.api.ClassNote.Controller;
 
 import java.util.List;
+import java.util.UUID;
+import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -55,4 +59,13 @@ public class ProfessorController {
 	 public ResponseEntity<List<ProfessorModel>> listarProfessores(){
 		 return ResponseEntity.status(HttpStatus.OK).body(_professorService.findAll());
 	 }
-}
+	 
+	 @GetMapping("/{id}")
+	 public ResponseEntity<Object> umProfessor(@PathVariable(value="id") UUID id){
+		 Optional<ProfessorModel> professorO = _professorService.findById(id);
+		 if(professorO.isPresent()) {
+			 return ResponseEntity.status(HttpStatus.OK).body(professorO.get());
+		 }
+		 return ResponseEntity.status(HttpStatus.NOT_FOUND).body("O professor nao existe!");
+	 }
+ }
