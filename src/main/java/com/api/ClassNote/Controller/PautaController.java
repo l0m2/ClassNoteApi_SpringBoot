65fd572aaa470dd45754c245576fde5eb21cb9a1
Leopoldo Jacob Matsinhe <121976617+l0m2@users.noreply.github.com>
2023-10-03 11:59:51 +0200

    put pauta

--- src/main/java/com/api/ClassNote/Controller/PautaController.java
+++ src/main/java/com/api/ClassNote/Controller/PautaController.java
@@ -9,9 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -68,5 +70,31 @@ public class PautaController {
 	  }
 	  return ResponseEntity.status(HttpStatus.NOT_FOUND).body("O id da pauta fornecido nao existe");
 	 }
-	
+	 
+	 @DeleteMapping("/{id}")
+	 public ResponseEntity<Object> deletePauta(@PathVariable (value = "id") UUID id){
+		  pautaO = pautaService.findById(id);
+		  if(pautaO.isPresent()) { 
+		pautaService.delete(pautaO.get());
+		return ResponseEntity.status(HttpStatus.OK).body("Apagado com sucesso");
+		  }
+	return ResponseEntity.status(HttpStatus.NOT_FOUND).body("O id da pauta fornecido nao existe");
+	 }
+	 
+	 @PutMapping("/{id}")
+	 public ResponseEntity<Object> atualizaPauta(@PathVariable (value = "id") UUID id, @RequestBody @Valid PautaDTO pautaD){
+		 pautaO = pautaService.findById(id);
+		  if(pautaO.isPresent()) {  
+			  alunoO = alunoService.findById(pautaD.getAluno_id());
+		if(alunoO.isPresent()) {
+		BeanUtils.copyProperties(pautaD, pautaM);
+		pautaM.setAluno(alunoO.get());
+		pautaM.setId(alunoO.get().getId());
+		pautaService.save(pautaM);
+		return ResponseEntity.status(HttpStatus.OK).body(pautaService.save(pautaM));
+				 }
+	return ResponseEntity.status(HttpStatus.NOT_FOUND).body("O id do aluno fornecido nao existe");		 
+	 }
+ return ResponseEntity.status(HttpStatus.NOT_FOUND).body("O id da pauta fornecido nao existe");
+}
 }
